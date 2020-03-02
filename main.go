0c1e1693eb5044cf604d3b832a71ace9ea867205
Stefan Truong <stefan.truong@gmail.com>
2020-03-02 14:03:38 +0100

    add simple http server

--- main.go
+++ main.go
@@ -1,1 +1,13 @@
-package go_server
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("public"))))
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
